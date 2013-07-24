c0a131738bae35dc58adb0b6a9444fe0ceb1f128
Karim Jiwani <karimjiwani@Karims-Mac.local>
2013-07-24 14:35:29 -0400

    Initial Commit

---
+++ LocalList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocalList
+//
+//  Created by Karim Jiwani on 2013-07-24.
+//  Copyright (c) 2013 Karim Jiwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
